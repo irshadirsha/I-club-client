7d9026890be72562b9f2b1e5422cd218560f27e7
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-09-06 21:32:54 +0530

    chat

--- src/components/User/Club/ClubHome.jsx
+++ src/components/User/Club/ClubHome.jsx
@@ -1,3 +1,4 @@
+
 import React,{useEffect, useRef, useState} from 'react';
 import {  useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../../../../Api/config';
@@ -45,19 +46,23 @@ const [errors, setErrors] = useState({
           scrollableRef.current.scrollTop = scrollableRef.current.scrollHeight;
         }
       }, [showmessage]);
+      useEffect(()=>{
+        fetchdata()
+        fetchevent()
+        },[])
       
 useEffect(()=>{
-  fetchdata()
-  fetchevent()
   fetchmessage()
-  socket.on('chatMessage', (newMessage) => {
-    console.log('Received chatMessage:', newMessage);
-    setShowMessage((prevMessages) => [...prevMessages, newMessage]);
-  });  
+    socket.on("chatMessage", (newMessage) => {
+      setMessages((prevMessages) => [
+        ...(prevMessages?.length ? prevMessages : []),
+        newMessage,
+      ]);
+    });
   return () => {
     socket.disconnect();
   };
-},[])
+})
 const fetchevent=async()=>{
     const {data}= await axiosInstance.post('/get-event',{clubName})
     console.log("------------enents---------------------",data)
@@ -463,18 +468,25 @@ export default ClubHome;
 
 
 
-// import React,{useEffect, useState} from 'react';
-// import axios from 'axios';
-// import { useLocation, useNavigate } from 'react-router-dom';
+
+
+
+
+
+// import React,{useEffect, useRef, useState} from 'react';
+// import {  useNavigate } from 'react-router-dom';
 // import { axiosInstance } from '../../../../Api/config';
 // import ClubNav from './ClubNav';
 // import { useSelector } from 'react-redux';
 // import { ToastContainer,toast } from 'react-toastify'
 // import io from 'socket.io-client'
 // import {ServerPort } from '../../../../Api/Serverport'
+// import Swal from 'sweetalert2';
+// import Loader from '../../Loader/Loader';
 // function ClubHome() {
+//     const scrollableRef = useRef(null);
 //     const socket = io(ServerPort);
-//     console.log("sssss",socket);
+//     console.log("socket-----------------------------------------------------",socket);
 //        const user=useSelector((state)=>state.user)
 //         const clubName=user.clubName
 //         const currentuser=user.id
@@ -483,6 +495,7 @@ export default ClubHome;
 
 //         console.log(clubName);
 //        const navigate=useNavigate()       
+//        const [loading, setLoading] = useState(true);
 //        const [role,setRole]=useState('')
 //        const [clubData,setClubData]=useState(null)
 //        const [messages,setMessages]=useState({
@@ -490,6 +503,11 @@ export default ClubHome;
 //      time:"",
 //      location:""
 // })
+// const [errors, setErrors] = useState({
+//   message:"",
+//   time:"",
+//   location:""
+// });
 //      const [events,SetEvents]=useState([])
 
 //      const [chatMessage, setChatMessage] = useState('');
@@ -497,17 +515,23 @@ export default ClubHome;
      
 //      console.log(showmessage);
 
-
+//      useEffect(() => {
+//         if (scrollableRef.current) {
+//           scrollableRef.current.scrollTop = scrollableRef.current.scrollHeight;
+//         }
+//       }, [showmessage]);
+      
 // useEffect(()=>{
-//      fetchdata()
-//      fetchevent()
-//      fetchmessage()
-//      socket.on('chatMessage', (newMessage) => {
-//         setShowMessage((prevMessages) => [...prevMessages, newMessage]);
-//       });
-//       return () => {
-//         socket.disconnect();
-//       };
+//   fetchdata()
+//   fetchevent()
+//   fetchmessage()
+//   socket.on('chatMessage', (newMessage) => {
+//     console.log('Received chatMessage:', newMessage);
+//     setShowMessage((prevMessages) => [...prevMessages, newMessage]);
+//   });  
+//   return () => {
+//     socket.disconnect();
+//   };
 // },[])
 // const fetchevent=async()=>{
 //     const {data}= await axiosInstance.post('/get-event',{clubName})
@@ -524,11 +548,21 @@ export default ClubHome;
 //     setClubData(data.data)
 //     console.log(data.userRole);
 //     setRole(data.userRole)
+//     setLoading(false)
 // }
 
 // const handleEventSubmit=async(e)=>{
 //     e.preventDefault()
 //     try {
+//       if (messages.message.trim() === "" && messages.time.trim() === "" && messages.location.trim() === "" ) {
+//         setErrors({
+//           ...errors,
+//           message: "Enter Your Events",
+//           time: "Enter Your Event Time",
+//           location: "Enter Your Event Location",
+//         });
+//         return;
+//       }
 //         console.log("eventss",messages,clubData?._id,);
 //         const {data} =await axiosInstance.post('/add-events',{messages,club:clubData?._id})
 //         console.log("eventss---",data);
@@ -549,17 +583,28 @@ export default ClubHome;
 
 // const deleteEvent=async( id)=>{
 //     try {
-//         console.log("delete id",id)
-//        const {data}=await axiosInstance.post('/delete-event',{id:id})
-//        console.log(data)
-//        fetchevent()
-//        if (data.message) {
-//         toast.success(data.message)
-//     }
+//         // e.preventDefault()
+//         Swal.fire({
+//             title: 'Delete Event Confirmation',
+//             text: 'Are you sure you want to remove the Event?',
+//             icon: 'warning',
+//             showCancelButton: true,
+//             confirmButtonText: 'Yes, Delete!',
+//             cancelButtonText: 'Cancel',
+//           }).then(async (result) => {
+//             if (result.isConfirmed) {
+//               console.log('leave', clubName);
+//               const {data}=await axiosInstance.post('/delete-event',{id:id})
+//               fetchevent()
+//               if (response.data.message) {
+//                 Swal.fire('Success', response.data.message, 'success');
+//               }
+             
+//             }
+//           });
 //     } catch (error) {
-        
-//     }
-   
+//         console.log("error in event deletion");
+//     }  
 // }
 
 // const handleSendMessage = async () => {
@@ -570,7 +615,7 @@ export default ClubHome;
 //         clubName: clubName,
 //       });
 //       socket.emit('chatMessage', { text: chatMessage, clubName: clubName });
-    
+//       console.log('Sent chatMessage:', chatMessage);
 //     setChatMessage('');
 //     fetchmessage();
 //     console.log(data);
@@ -585,22 +630,29 @@ export default ClubHome;
 
 //     setShowMessage(response.data.response)
 //     console.log(response);
-
 // }
+
+// const handleKeyDown = (event) => {
+//   if (event.key === 'Enter' && !event.shiftKey) {
+//     event.preventDefault(); 
+//     handleSendMessage();
+//   }
+// };
 // return (
 // <div>
+// {loading && <Loader/>}
 // <ClubNav state={clubName}/>
 // <section className="pt-16 bg-primary">
 // <div className="container mx-auto">
 //     <div className="flex flex-col md:flex-row items-center">
-//         <div className="md:w-6/12 md:text-left text-center py-6 md:mt-3">
+//         <div className=" md:w-6/12 md:text-left text-center py-6 md:mt-3">
 //             <div className="text-center md:text-start md:pl-6 pl-4 pr-8 md:ml-8 md:mt-5">
 //                 <h2 className="font-normal text-2xl text-black-600 mb-3">
 //                     "Unites integrity, compassion, shared values; making a positive impact on the community with unity and purposeful actions."
 //                 </h2>
 //                 <h1 className="text-4xl font-mono md:pl-8 md:text-start font-semibold mb-4">{clubData ? clubData.clubName : clubName}</h1>
-//                 <p className="font-medium text m-4">
-//                 {clubData?.clubData?.about}
+//                 <p className="font-medium text-lg m-4">
+//                 {clubData?.category}
 //                 </p>
 //                 <div className="mt-4 flex justify-start items-start">
 //                     <button 
@@ -614,7 +666,7 @@ export default ClubHome;
 //         </div>
 
 //         <div className=" md:w-6/12 md:order-0 order-1 md:text-center mt-5 md:mt-0">
-//     <div className="rounded-lg p-4 overflow-hidden event-style">
+//     <div className=" rounded-lg p-4 overflow-hidden event-style">
 //     <div className="relative w-full h-full">
 //             <div className="relative top-0 right-0 bottom-0 left-0 rounded-lg overflow-hidden">
 //                 <img
@@ -629,40 +681,33 @@ export default ClubHome;
 
 //     </div>
 
-//     <div className="flex flex-col pt-12 md:flex-row items-center">
+//     <div className=" flex flex-col pt-12 md:flex-row items-center">
 // {/* ITS CHAT MESSAGES */}
-//      <div className=" md:w-6/12 md:text-left text-center p-8 py-2 md:mt-3">
-// <div className="col-sm-6">
+//      <div className=" w-full  md:w-6/12 md:text-left text-center p-4 py-2 md:mt-3">
+// <div className=" col-sm-6">
 //   <h1 className=" text-4xl text-center px-2 font-semibold font-mono">Chat</h1>
-//   <section className="  rounded-lg shadow-md overflow-hidden">
+//   <section className=" md:px-8 overflow-hidden">
 //     <div className="card">
 //       <div className="bg-blue-200 card-header  flex justify-between items-center p-3 border-t-4 border-blue-700 ">
 //         <h5 className="text-md font-semibold">Chat messages</h5>
 //         <h5 className="text-lg font-bold">{clubName}</h5>
 //         <div className="flex flex-row items-center space-x-3">
-//           <div className="nav-item dropdown relative">
-//             {/* <button className="d-inline-block py-2 px-3 text-black text-sm font-medium dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//               Active members
-//             </button> */}
-//             <ul className="dropdown-menu dropdown-menu-end border-0 shadow-lg rounded-md bg-white" aria-labelledby="navbarDropdown">
-//               {/* <li><a className="dropdown-item">User 1</a></li> */}
-//               {/* Add more active users here */}
-//             </ul>
-//           </div>
-//           <i className="fas fa-minus text-gray-500 text-xs"></i>
-//           <i className="fas fa-comments text-gray-500 text-xs"></i>
 //           <i className="fas fa-times text-gray-500 text-xs"></i>
 //         </div>
 //       </div>
-//       <div className="bg-gray-300 card-body  border overflow-y-auto relative" style={{ height: '400px' }}>
+      
+//       <div className="bg-gray-300 card-body border overflow-y-auto relative" style={{ height: '400px' }} ref={scrollableRef}>
 //     {showmessage?.map((message, index) => (
 //         (message?.user?._id !== currentuser) ? (
 //             <div key={index}>   
-//             {/* <h1>{currentuser}</h1>
+//             {/* <h1>....{currentuser}</h1>
 //             <h1>{message?.user?._id }</h1> */}
-//                 {/* <div className="flex justify-between">
-//                     <p className="small mb-0">{message?.user?.username}</p>
-//                 </div> */}
+//              {/* {index === 0 || new Date(message.time).toLocaleDateString() !== new Date(showmessage[index - 1]?.time).toLocaleDateString() ? (
+//                 <p className="text-center text-xs text-gray-500">
+//                   {new Date(message.time).toLocaleDateString()}
+//                 </p>
+//               ) : null} */}
+
 //                 <div className="flex flex-row items-center mx-2 pt-4 space-x-2">
 //                     <div className="w-12 h-12 rounded-full overflow-hidden">
 //                         <img
@@ -714,16 +759,20 @@ export default ClubHome;
 //             placeholder='enter message...'
 //             value={chatMessage}
 //           onChange={(e) => setChatMessage(e.target.value)}
+//           onKeyDown={handleKeyDown}
 //             aria-label="Recipient's username"
 //             aria-describedby="button-addon2"
 //           />
 //           <button
-//             className="bg-primary mx-2 text-white py-1 px-6 rounded-md"
+//             className="bg-primary mx-1 text-white py-1 px-6 rounded-md hover:border"
 //             type="button"
 //             id="button-addon2"
 //             onClick={handleSendMessage}
 //           >
-//             Send
+//             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+//   <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+// </svg>
+
 //           </button>
 //         </div>
 //       </div>
@@ -732,50 +781,75 @@ export default ClubHome;
 // </div>
 // </div>
 //     {/* EVENT HANDLING */}
-//         <div className="  md:w-6/12 md:text-left text-center  py-6 md:mt-3">
+//         <div className=" w-full p-4 block md:w-6/12 md:text-left text-center  py-6 md:mt-3">
 //             <div className="  text-center md:text-start md:pl-4  md:pr-8  md:ml-8 md:mt-5">
 //                 <h1 className="text-4xl font-mono md:pl-8 md:text-center font-semibold mb-4">Events</h1>
-//                 <div className=" p-1  mb-2 responsive-form">
+//                 <div className=" p-1   mb-2 responsive-form">
 //                     {/* i need to show input div only if user secretiry or presidenrt */}  
 //                         { (role === 'president' || role === 'secretory') && (   
 //                     <div className="p-3 mb-2 bg-gray-300 rounded-3xl  border-current border">
 //                         <form >
-//                         <div className="form-outline mb-4 text-center pt-4">
-//                         <input
-//                     type="text"
-//                     id="form3Example4"
-//                     name="message"
-//                     value={messages.message}
-//                     onChange={(e) => setMessages({...messages,[e.target.name]: e.target.value})}        
-//                     className="form-control p-2  w-4/6 drop-shadow-md rounded-lg  border-current border outline-slate-300 "
-//                        />
-//                     <br></br>
-//                     <label className="form-label" htmlFor="form3Example4">Enter Events</label>
-//                       </div>
-//                       <div className="form-outline mb-4 text-center pt-4">
-//                         <input
-//                     type="text"
-//                     id="form3Example5"
-//                     name="location"
-//                     value={messages.location}
-//                     onChange={(e) => setMessages({...messages,[e.target.name]: e.target.value})}        
-//                     className="form-control p-2  w-4/6 drop-shadow-md rounded-lg  border-current border outline-slate-300 "
-//                        />
-//                     <br></br>
-//                     <label className="form-label" htmlFor="form3Example4">Enter Events Location</label>
-//                       </div>
-//                       <div className="form-outline mb-4 text-center pt-4">
-//                         <input
-//                     type="text"
-//                     id="form3Example7"
-//                     name="time"
-//                     value={messages.time}
-//                     onChange={(e) => setMessages({...messages,[e.target.name]: e.target.value})}        
-//                     className="form-control p-2  w-4/6 drop-shadow-md rounded-lg  border-current border outline-slate-300 "
-//                        />
-//                     <br></br>
-//                     <label className="form-label" htmlFor="form3Example4">Enter Events Time</label>
-//                       </div>
+
+//                         <div className="form-outline mb-2 text-center pt-1">
+//                 <label htmlFor="form3Example1" className="block text-gray-800  text-md mb-1">
+//                  Events
+//                 </label>
+//                 <input
+//                   type="text"
+//                   id="form3Example1"
+//                   name="message"
+//                   value={messages.message}
+//                   onChange={(e) => {
+//                     setMessages({...messages,[e.target.name]: e.target.value})
+//                     setErrors({});
+//                   }}
+//                   className={`form-control p-2 w-4/6 drop-shadow-md rounded-lg border-current border outline-slate-300 ${
+//                     errors.message && "border-red-500"
+//                   }`}
+//                 />
+//                 {errors.message && <p className="text-red-500">{errors.message}</p>}
+//               </div>
+
+//               <div className="form-outline mb-2 text-center pt-1">
+//                 <label htmlFor="form3Example1" className="block text-gray-800  text-md mb-1">
+//                 Enter Events Location
+//                 </label>
+//                 <input
+//                   type="text"
+//                   id="form3Example2"
+//                   name="location"
+//                   value={messages.location}
+//                   onChange={(e) => {
+//                     setMessages({...messages,[e.target.name]: e.target.value})
+//                     setErrors({});
+//                   }}
+//                   className={`form-control p-2 w-4/6 drop-shadow-md rounded-lg border-current border outline-slate-300 ${
+//                     errors.location && "border-red-500"
+//                   }`}
+//                 />
+//                 {errors.location && <p className="text-red-500">{errors.location}</p>}
+//               </div>
+            
+//               <div className="form-outline mb-2 text-center pt-1">
+//                 <label htmlFor="form3Example1" className="block text-gray-800  text-md mb-1">
+//                 Enter Events Time
+//                 </label>
+//                 <input
+//                   type="text"
+//                   id="form3Example3"
+//                   name="time"
+//                   value={messages.time}
+//                   onChange={(e) => {
+//                     setMessages({...messages,[e.target.name]: e.target.value})
+//                     setErrors({});
+//                   }}
+//                   className={`form-control p-2 w-4/6 drop-shadow-md rounded-lg border-current border outline-slate-300 ${
+//                     errors.time && "border-red-500"
+//                   }`}
+//                 />
+//                 {errors.time && <p className="text-red-500">{errors.time}</p>}
+//               </div>    
+                      
 //                             <div className='bg-primary text-center rounded-2xl p-3'>
 //                             <button onClick={handleEventSubmit} type="submit">Submit</button>
 //                             </div>
@@ -845,14 +919,6 @@ export default ClubHome;
 // }
 
 // export default ClubHome;
-
-
-
-
-
-
-
-
 
 
 
