afc1ee120898869f55b6a44b3dba8cd74278677f
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-10-16 18:37:03 +0530

    nav

--- src/components/User/UserHome/HomeNav.jsx
+++ src/components/User/UserHome/HomeNav.jsx
@@ -60,7 +60,7 @@ const HomeNav = () => {
                 </NavLink>
               </li>
               <li>
-                <NavLink to='/about' className="block py-2 pl-3 pr-4 text-black rounded md:border-0 md:hover:text-blue-700 md:p-0  "exact>
+                <NavLink to='/about' className="block py-2 pl-3 pr-4 text-gray-900 rounded md:border-0 md:hover:text-blue-700 md:p-0  "exact>
                   About
                   </NavLink>
               </li>
@@ -117,13 +117,13 @@ const HomeNav = () => {
               </li>):(
                 <>
               <li>
-                <NavLink to='/login' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact>
+                <NavLink to='/login' className="block py-2 pl-3 pr-4 text-gray-900 rounded  md:border-0 md:hover:text-blue-700 md:p-0" exact>
                   Login
                 </NavLink>
               </li>
               <li>
 
-                <NavLink to='/signup' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact>
+                <NavLink to='/signup' className="block py-2 pl-3 pr-4 text-gray-900 rounded  md:border-0 md:hover:text-blue-700 md:p-0" exact>
                   SignIn
                 </NavLink>
               </li>
@@ -256,3 +256,16 @@ export default HomeNav;
     </NavLink>
 </li>  */}
 
+// </li>):(
+//   <>
+// <li>
+//   <NavLink to='/login' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact>
+//     Login
+//   </NavLink>
+// </li>
+// <li>
+
+//   <NavLink to='/signup' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact>
+//     SignIn
+//   </NavLink>
+// </li>
