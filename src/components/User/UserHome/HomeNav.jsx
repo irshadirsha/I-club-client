e37fba8cd8f327454e2ba29c2369847d3c27584f
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-10-16 18:25:35 +0530

    navbar

--- src/components/User/UserHome/HomeNav.jsx
+++ src/components/User/UserHome/HomeNav.jsx
@@ -55,12 +55,12 @@ const HomeNav = () => {
             {/* <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"> */}
             {/* dark:bg-gray-800 md:dark:bg-gray-900  dark:border-gray-700 */}
               <li>
-                <NavLink to='/' className="block py-2  pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page" exact>
+                <NavLink to='/' className="block py-2  pl-3 pr-4 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page" exact>
                   Home
                 </NavLink>
               </li>
               <li>
-                <NavLink to='/about' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"exact>
+                <NavLink to='/about' className="block py-2 pl-3 pr-4 text-black rounded md:border-0 md:hover:text-blue-700 md:p-0  "exact>
                   About
                   </NavLink>
               </li>
@@ -242,4 +242,17 @@ export default HomeNav;
 
 // export default HomeNav
 
+
+
+
+{/* <li>
+  <NavLink to='/' className="block py-2  pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page" exact>
+    Home
+  </NavLink>
+</li>
+<li>
+  <NavLink to='/about' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"exact>
+    About
+    </NavLink>
+</li>  */}
 
